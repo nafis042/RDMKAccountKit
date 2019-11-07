8758c7f93b9a0e60e1a97f9176a1b8adbaca0434
nafis042 <nafis00001@gmail.com>
2019-11-07 16:42:23 +0600

    Initial Commit

---
+++ RDMKAccountKit/RDMKAccountKit.h
@@ -0,0 +1,19 @@
+//
+//  RDMKAccountKit.h
+//  RDMKAccountKit
+//
+//  Created by Nafis Islam on 7/11/19.
+//  Copyright © 2019 Nafis Islam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RDMKAccountKit.
+FOUNDATION_EXPORT double RDMKAccountKitVersionNumber;
+
+//! Project version string for RDMKAccountKit.
+FOUNDATION_EXPORT const unsigned char RDMKAccountKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RDMKAccountKit/PublicHeader.h>
+
+
